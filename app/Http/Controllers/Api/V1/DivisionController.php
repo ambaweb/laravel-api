332c90f550d3da269abc0eebcdf33368c8ab27e9
Dhanabal Duraisamy <info@ambawebworks.com>
2022-04-05 11:20:23 -0400

    git re-indexed

--- app/Http/Controllers/Api/V1/DivisionController.php
+++ app/Http/Controllers/Api/V1/DivisionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\V1\DivisionCollection;
 use App\Models\Division;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\BuilderResource;
+use App\Http\Resources\V1\DivisionCollection;
 
 class DivisionController extends Controller
 {
@@ -37,7 +38,7 @@ class DivisionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return new BuilderResource(Division::create($request->all()));
     }
 
     /**
@@ -46,9 +47,9 @@ class DivisionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Division $division)
     {
-        //
+        return new BuilderResource($division);
     }
 
     /**
