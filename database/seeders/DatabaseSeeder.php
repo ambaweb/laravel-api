7ea11b364434c3236300c2ffee678db7b3c5f9af
Dhanabal Duraisamy <info@ambawebworks.com>
2022-04-07 00:14:47 -0400

    sedding demo user for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Builder;
 use App\Models\Division;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -17,10 +18,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // create demo admin user
+        User::factory()->create([
+            'name' => 'Laravel Admin',
+            'email' => env('API_TEST_USER_NAME'),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10)
+        ]);
+
         // \App\Models\User::factory(10)->create();
-        User::factory(2)->create();
         Builder::factory(2)->create();
         Division::factory(4)->create();
-        
     }
 }
